84111c560294d1e92cbd6915831816c82b69e02e
ryanwei <rainmanwy@gmail.com>
2022-07-18 16:21:21 +0800

    test

--- 05_sql/rollout.sh
+++ 05_sql/rollout.sh
@@ -14,7 +14,7 @@ for i in ${PWD}/*.${BENCH_ROLE}.*.sql; do
 		sql_id=$(echo ${i} | awk -F '.' '{print $3}')
 		echo ${sql_id}
 		start_log
-		if [ "${EXPLAIN_ANALYZE}" == "false" || "${sql_id}" == "15" ]; then
+		if [ "${EXPLAIN_ANALYZE}" == "false" -o "${sql_id}" == "15" ]; then
 			log_time "psql -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE=\"\" -f ${i} | wc -l"
 			tuples=$(psql -v ON_ERROR_STOP=1 -A -q -t -P pager=off -v EXPLAIN_ANALYZE="" -f ${i} | wc -l; exit ${PIPESTATUS[0]})
 		else
